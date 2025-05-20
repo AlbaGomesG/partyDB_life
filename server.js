28a93bf6d93d7b704833a067dc782d2e51b90681
Luiz_Henrique <luizaureliano.tds1.senai@gmail.com>
2025-05-20 09:50:43 -0300

    Feat: create user auth

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+
 const placesRoutes = require("./src/routes/placesRoutes");
 const attractionsRoutes = require("./src/routes/attractionsRoutes");
 const stylesRoutes = require("./src/routes/stylesRoutes");
 const usersRoutes = require("./src/routes/usersRoutes");
-const path = require("path");
+const authRoutes = require("./src/routes/authRoutes");
 
+
+const path = require("path");
 const app = express();
+
+
 app.use(cors());
 app.use(express.json());
-
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api/attractions", attractionsRoutes);
 app.use("/api/styles", stylesRoutes);
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
