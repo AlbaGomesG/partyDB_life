92e6abc4f5f92bac9e9e8a669842b8ee985fcd05
maria parma <mariaparma.tds1.senai@gmail.com>
2025-05-27 09:37:18 -0300

    fix: config api

--- src/config/apiKey.js
+++ src/config/apiKey.js
@@ -1,12 +1,12 @@
 const apiKeyMiddleware = (req, res, next) => {
-    const clientKey = req.headers['x-api-key'];
-    const serverKey = process.env.API_KEY;
+    const apiKey = req.headers['x-api-key']; 
+    const serverKey = process.env.API_KEY; 
 
-    if (!clientKey || clientKey !== serverKey) { 
+    if (!apiKey || apiKey !== serverKey) { 
         return res.status(403).json({ error: 'Chave de API inválida ou ausente' });
     }
 
-    next();
+    next(); 
 };
 
 module.exports = apiKeyMiddleware;
