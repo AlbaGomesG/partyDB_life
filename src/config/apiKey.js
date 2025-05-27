f3b49f33bc625267c2030431e1a8dd5214d28c08
maria parma <mariaparma.tds1.senai@gmail.com>
2025-05-27 09:20:34 -0300

    fix: update in apikey

--- src/config/apiKey.js
+++ src/config/apiKey.js
@@ -1,12 +1,12 @@
 const apiKeyMiddleware = (req, res, next) => {
-    const apiKey = req.headers['x-api-key'];
+    const clientKey = req.headers['x-api-key'];
     const serverKey = process.env.API_KEY;
-    
-    if (!clientKey || cilentKey !== serverKey) {
+
+    if (!clientKey || clientKey !== serverKey) { 
         return res.status(403).json({ error: 'Chave de API inválida ou ausente' });
     }
-    
-    next();
-    };
 
-    module.exports = apiKeyMiddleware;
+    next();
+};
+
+module.exports = apiKeyMiddleware;
