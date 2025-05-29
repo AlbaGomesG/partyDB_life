007bbd01bbb72f594a37c4ccc340be20dfe64dca
maria parma <mariaparma.tds1.senai@gmail.com>
2025-05-29 08:50:17 -0300

    feat: delete controller, models and routes  of integrantes, places, styles and attractions

--- server.js
+++ server.js
@@ -2,13 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const placesRoutes = require("./src/routes/placesRoutes");
-const attractionsRoutes = require("./src/routes/attractionsRoutes");
-const stylesRoutes = require("./src/routes/stylesRoutes");
+
 const usersRoutes = require("./src/routes/usersRoutes");
 const authRoutes = require("./src/routes/authRoutes");
 const pool = require("./src/config/database");
-const integrantesRoutes = require("./src/routes/integrantesRoutes");
 const eventsRoutes = require("./src/routes/eventsRoutes");
 const postsRoutes = require("./src/routes/postsRoutes");
 const apiKeyMiddleware = require ("./src/config/apiKey")
@@ -21,15 +18,13 @@ app.use(apiKeyMiddleware)
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/places", placesRoutes);
+
 app.use("/api/users", usersRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/attractions", attractionsRoutes);
-app.use("/api/integrantes", integrantesRoutes);
 app.use("/api/events", eventsRoutes);
 app.use("/api/posts", postsRoutes);
 
-app.use("/api/styles", stylesRoutes);
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const PORT = process.env.PORT || 4000;
