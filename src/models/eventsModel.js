b76ffeb5deacec726fe326e8ca054a129c4c8be8
maria parma <mariaparma.tds1.senai@gmail.com>
2025-06-03 08:35:20 -0300

    feat: changing folder names

--- src/models/eventsModel.js
+++ src/models/eventsModel.js
@@ -15,21 +15,22 @@ const getEventById = async (id) => {
     return result.rows[0];
 };
 
-const createEvent = async (title, place_id, attraction_id, style_id, time_start, time_end, description,event_photo, events_rules, party_itens, take_products, hashtags) => {
-        const result = await pool.query(
-        "INSERT INTO events (title, place_id, attraction_id, style_id, time_start, time_end, description, event_photo, events_rules, party_itens, take_products, hashtags) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *",
-        [title, place_id, attraction_id, style_id, time_start, time_end, description, event_photo, events_rules, party_itens, take_products, hashtags]
+const createEvent = async (title, local, time_start, time_end, description, event_photo, events_rules, party_itens, take_products, attractions, styles, hashtags) => {
+    const result = await pool.query(
+        "INSERT INTO events (title, local, time_start, time_end, description, event_photo, events_rules, party_itens, take_products, attractions, styles, hashtags) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *",
+        [title, local, time_start, time_end, description, event_photo, events_rules, party_itens, take_products, attractions, styles, hashtags]
     );
     return result.rows[0];
 };
 
-const updateEvent = async (id, title, place_id, attraction_id, style_id, time_start, time_end, description, event_photo, events_rules, party_itens, take_products, hashtags) => {
+const updateEvent = async (id, title, local, time_start, time_end, description, event_photo, events_rules, party_itens, take_products, attractions, styles, hashtags) => {
     const result = await pool.query(
-        "UPDATE events SET title = $1, place_id = $2, attraction_id = $3, style_id = $4, time_start = $5, time_end = $6, description = $7, event_photo = $8, events_rules = $9, party_itens = $10, take_products = $11, hashtags = $12 WHERE id = $13 RETURNING *",
-        [title, place_id, attraction_id, style_id, time_start, time_end, description, event_photo, events_rules, party_itens, take_products, hashtags, id]
+        "UPDATE events SET title = $1, local = $2, time_start = $3, time_end = $4, description = $5, event_photo = $6, events_rules = $7, party_itens = $8, take_products = $9, attractions = $10, styles = $11, hashtags = $12 WHERE id = $13 RETURNING *",
+        [title, local, time_start, time_end, description, event_photo, events_rules, party_itens, take_products, attractions, styles, hashtags, id]
     );
     return result.rows[0];
 };
+
 const deleteEvent = async (id) => {
     const result = await pool.query("DELETE FROM events WHERE id = $1 RETURNING *", [id]);
     if (result.rowCount === 0) {
