fbcb420149062a3a04c179cae85974dec269c27c
AlbaGomesG <162628812+AlbaGomesG@users.noreply.github.com>
2025-06-10 11:15:04 -0300

    fix: arrumando a tabela de eventos, adicionando as imagens necessárias

--- src/models/eventsModel.js
+++ src/models/eventsModel.js
@@ -21,24 +21,24 @@ const getEventById = async (id) => {
     return result.rows[0];
 };
 
-const createEvent = async (title, local, time_start, time_end, description, event_photo, events_rules, party_itens, take_products, attractions, attractions_name, styles, styles_name, hashtags) => {
+const createEvent = async (title, local, time_start, time_end, photo, description, events_rules, party_itens, take_products, attractions, attractions_name, styles, styles_name, hashtags) => {
     if (!title || !local || !time_start || !time_end) {
         throw new Error("Os campos obrigatórios não foram preenchidos.");
     }
     const result = await pool.query(
-        "INSERT INTO events (title, local, time_start, time_end, description, event_photo, events_rules, party_itens, take_products, attractions, attractions_name, styles, styles_name, hashtags) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *",
-        [title, local, time_start, time_end, description, event_photo, events_rules, party_itens, take_products, attractions, attractions_name, styles, styles_name, hashtags]
+        "INSERT INTO events (title, local, time_start, time_end, photo, description, events_rules, party_itens, take_products, attractions, attractions_name, styles, styles_name, hashtags) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *",
+        [title, local, time_start, time_end, photo, description, events_rules, party_itens, take_products, attractions, attractions_name, styles, styles_name, hashtags]
     );
     return result.rows[0];
 };
 
-const updateEvent = async (id, title, local, time_start, time_end, description, event_photo, events_rules, party_itens, take_products, attractions, attractions_name, styles, styles_name, hashtags) => {
+const updateEvent = async (id, title, local, time_start, time_end, photo, description, events_rules, party_itens, take_products, attractions, attractions_name, styles, styles_name, hashtags) => {
     if (!id) {
         throw new Error("ID do evento é obrigatório.");
     }
     const result = await pool.query(
-        "UPDATE events SET title = $1, local = $2, time_start = $3, time_end = $4, description = $5, event_photo = $6, events_rules = $7, party_itens = $8, take_products = $9, attractions = $10, attractions_name = $11, styles = $12, styles_name = $13, hashtags = $14 WHERE id = $15 RETURNING *",
-        [title, local, time_start, time_end, description, event_photo, events_rules, party_itens, take_products, attractions, attractions_name, styles, styles_name, hashtags, id]
+        "UPDATE events SET title = $1, local = $2, time_start = $3, time_end = $4, description = $5, photo = $6, events_rules = $7, party_itens = $8, take_products = $9, attractions = $10, attractions_name = $11, styles = $12, styles_name = $13, hashtags = $14 WHERE id = $15 RETURNING *",
+        [title, local, time_start, time_end, photo, description, events_rules, party_itens, take_products, attractions, attractions_name, styles, styles_name, hashtags, id]
     );
     if (result.rowCount === 0) {
         return { error: "Evento não encontrado para atualização!" };
--- src/routes/eventsRoutes.js
+++ src/routes/eventsRoutes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const eventsController = require("../controllers/eventsController");
+const upload = require("../config/upload");
 
 router.get("/", eventsController.getAllEvents);
 router.get("/:id", eventsController.getEvent);
-router.post("/", eventsController.createEvent);
+router.post("/", upload.fields([
+    {name: "photo", maxCount: 1},
+    {name: "attractions", maxCount: 1},
+    {name: "styles", maxCount: 1},
+]), eventsController.createEvent);
 router.put("/:id", eventsController.updateEvent);
 router.delete("/:id", eventsController.deleteEvent);
 
